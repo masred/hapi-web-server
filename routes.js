2a3d1b2a4edfb755402fa26115e5c141938ee384
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-11-15 19:01:11 +0700

    initial commit

---
+++ routes.js
@@ -0,0 +1,53 @@
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Method is not allowed";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Method is not allowed";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if (lang === "id") {
+        return `Hai, ${name}!`;
+      }
+
+      return `Hello, ${name}!`;
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Page not found";
+    },
+  },
+];
+
+module.exports = routes;
